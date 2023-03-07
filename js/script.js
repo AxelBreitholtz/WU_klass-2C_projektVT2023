12fc033281d0124744da18c86bb8026c07e6265a
AxelBreitholtzNTIJohanneberg <axel.breitholtz@elev.ga.ntig.se>
2023-03-07 14:30:18 +0100

    vecka_10

--- js/script.js
+++ js/script.js
@@ -1,9 +1,20 @@
+var menu_visible = false
+
 function toggleMenu() {
 
     document.getElementById('link_list').classList.toggle('link_list_visible')
 
     document.getElementById('burger').classList.toggle('burger_x_form')
 
+    if (menu_visible) {
+        document.getElementById('nav').style.backgroundColor= "transparent";
+        menu_visible = false
+    }
+    else {
+        document.getElementById('nav').style.backgroundColor= "rgb(" + 0 + ", " + 63 + ", " + 94 + ")";
+        menu_visible = true
+    }    
 }
 
 document.getElementById('burger').addEventListener('click', toggleMenu)
+
